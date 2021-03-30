0c4f03ebc261f8325ef0c2ee3d08cb4c993c15ea
Naomi Que <149199@ocelot.ala.int.uts.edu.au>
2021-03-30 16:05:41 +1100

    Added new field for saving Substance Painter files

--- js/scripts.js
+++ js/scripts.js
@@ -20,11 +20,12 @@ const copyToClipboard = id => {
     
     var label = 'copied1'; 
     if (id === 'field_sp1') label = 'copied1';
-    else if (id === "field_sp2") label = 'copied2';
+    else if (id === "field_sp3") label = 'copied2';
     else if (id === "field_katana1") label = 'copied3';
     else if (id === "field_katana2") label = 'copied4';
     else if (id === "field_rv1") label = 'copied5';
     else if (id === "field_publish1") label = 'copied6';
+    else if (id === "field_sp2") label = 'copied7';
 
     var copyLabel = document.getElementById(label);
     copyLabel.className = "copied";
@@ -53,6 +54,7 @@ function populate() {
     var links=document.getElementById('links'); // Is this still being used?
     var field_sp1 = document.getElementById('field_sp1');
     var field_sp2 = document.getElementById('field_sp2');
+    var field_sp3 = document.getElementById('field_sp3');
     var field_katana2=document.getElementById('field_katana2');
     var field_rv1=document.getElementById('field_rv1');
     var field_publish1=document.getElementById('field_publish1');
@@ -60,6 +62,7 @@ function populate() {
     // Unlock field
     field_sp1.readonly = false;
     field_sp2.readonly = false;
+    field_sp3.readonly = false;
     field_katana2.readonly = false;
     field_rv1.readonly = false;
     field_publish1.readonly = false;
@@ -75,11 +78,18 @@ function populate() {
     if (version === '') {
         version='$VERSION';
     }
+    if (year === '') {
+        version='$YEAR';
+    }
+    if (studioCode === '') {
+        version='$STUDIO_CODE';
+    }
     if (submaterial === '') {
         submaterial='defaultMaterial';
     }
     var str = "";
     var spImport = `/mnt/ala/mav/${year}/jobs/${studioCode}/assets/${assetType}/${assetName}/model/model/caches/fbx`;
+    var spSave = `/mnt/ala/mav/${year}/wip/${studioCode}/assets/${assetType}/${assetName}/surfacing/surfacing/${name}/substancePainter/build`;
     var spExport = `/mnt/ala/mav/${year}/wip/${studioCode}/assets/${assetType}/${assetName}/surfacing/surfacing/${name}/katana/textures/${submaterial}`;
     var katanaUsdOpen = `/mnt/ala/mav/${year}/jobs/${studioCode}/assets/${assetType}/${assetName}/model/model/caches/usd`;
     var rvOpen = `/mnt/ala/mav/${year}/wip/${studioCode}/assets/${assetType}/${assetName}/surfacing/surfacing/${name}/katana/renders/default/${version}/primary/beauty/1920x1080/acescg/exr`;
@@ -87,7 +97,8 @@ function populate() {
 
     //links.innerHTML = `${asset}`;
     field_sp1.value = spImport;
-    field_sp2.value = spExport;
+    field_sp2.value = spSave;
+    field_sp3.value = spExport;
     field_katana2.value = katanaUsdOpen;
     field_rv1.value = rvOpen;
     field_publish1.value = publish1;
